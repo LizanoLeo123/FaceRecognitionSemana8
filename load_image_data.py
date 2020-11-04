c17f4e46920be7a567d41487265fea45faf8beee
LizanoLeo123 <leoln123@live.com>
2020-11-03 18:22:49 -0600

    Created Face rec model with PCA and added Full Dataset

--- load_image_data.py
+++ load_image_data.py
@@ -9,31 +9,34 @@ from pydrive.drive import GoogleDrive
 
 
 def load_local_images():
-    img_df =pd.DataFrame()
-    
+    img_df = pd.DataFrame()
+
     for person_dir in os.scandir(args["directory"]):
         if person_dir.is_dir():
             for file in os.scandir(person_dir.path):
-                if file.is_file() and (file.path.endswith(".jpg") or file.path.endswith(".png")):
+                if file.is_file() and (file.path.endswith(".jpg")
+                                       or file.path.endswith(".png")
+                                       or file.path.endswith(".jpeg")):
                     temp_df = load_local_image_to_df(file.path, person_dir.name)
                     if img_df.empty:
                         img_df = temp_df
                     else:
                         img_df = pd.concat((img_df, temp_df), ignore_index=True)
-    print(img_df.info())
+    print(img_df)
     img_df.to_csv(args["saveFile"], index=False)
 
+
 def load_cloud_images():
-    img_df =pd.DataFrame()
+    img_df = pd.DataFrame()
 
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
     drive = GoogleDrive(gauth)
-    
+
     dir_list = drive.ListFile({'q': "'1IhufSGU5Llbp_3pO8lw2A4g3cz12IlRB' in parents and trashed=false"}).GetList()
 
     for person_dir in dir_list:
-        file_list = drive.ListFile({'q': "'"+person_dir['id']+"' in parents and trashed=false"}).GetList()
+        file_list = drive.ListFile({'q': "'" + person_dir['id'] + "' in parents and trashed=false"}).GetList()
         for file in file_list:
             temp_df = load_cloud_image_to_df(file['id'], person_dir["title"])
             if img_df.empty:
@@ -45,21 +48,22 @@ def load_cloud_images():
 
 
 def load_local_image_to_df(img_source, target_name):
-    img_array = cv2.imread(img_source, cv2.IMREAD_GRAYSCALE)
+    img_array = cv2.resize(cv2.imread(img_source, cv2.IMREAD_GRAYSCALE), (100, 100))
     img_array = (img_array.flatten())
     img_array = img_array.reshape(-1, 1).T
-    df= pd.DataFrame(img_array)
+    df = pd.DataFrame(img_array)
     df['target'] = pd.Series(target_name, index=df.index)
     return df
 
+
 def load_cloud_image_to_df(img_id, target_name):
-    resp = urllib.request.urlopen("https://drive.google.com/uc?export=view&id="+ img_id)
+    resp = urllib.request.urlopen("https://drive.google.com/uc?export=view&id=" + img_id)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
-            
-    img_array = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+
+    img_array = cv2.resize(cv2.imread(image, cv2.IMREAD_GRAYSCALE), (100, 100))
     img_array = (img_array.flatten())
     img_array = img_array.reshape(-1, 1).T
-    df= pd.DataFrame(img_array)
+    df = pd.DataFrame(img_array)
     df['target'] = pd.Series(target_name, index=df.index)
     return df
 
@@ -67,15 +71,12 @@ def load_cloud_image_to_df(img_id, target_name):
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--directory",
-        default="D:\TEC\Patrones\Img_PCA",
-	help="path to image directory")
+                default=".\Dataset",
+                help="path to image directory")
 ap.add_argument("-sf", "--saveFile", type=str,
-	default="./data.csv",
-	help="path to the csv file to store the data")
+                default="./data.csv",
+                help="path to the csv file to store the data")
 args = vars(ap.parse_args())
 
-
-load_cloud_images()
-
-
-
+load_local_images()
+# load_cloud_images()
